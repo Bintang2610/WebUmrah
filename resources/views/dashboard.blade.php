0607090f34037ca11516cec73d7a4e7b98b2503e
Bintang2610 <hanifanbintang06@gmail.com>
2025-04-08 13:44:53 +0700

    updateeeee

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" type="image/png" href="{{ asset('logobiru.png') }}">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         @vite('resources/css/app.css')
